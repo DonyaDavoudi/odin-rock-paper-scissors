361af854aca69d6dbd82aee97f8a6d499c099826
Donya Davoudi <donya.davoodi24@gmail.com>
2024-07-24 20:25:51 +0330

    getComputerChoice Function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+function getComputerChoice(){
+    const min = 1;
+    const max = 3;
+    const diff = max - min;
+    const random = (Math.round(Math.random() * diff + min));
+    if(random === 1){
+        return "rock";
+    } else if(random === 2){
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
+
+console.log(getComputerChoice());
