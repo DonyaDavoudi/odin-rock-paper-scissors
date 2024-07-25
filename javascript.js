345fed801bc098d0799847d1127d9fe5cb716ca1
Donya Davoudi <donya.davoodi24@gmail.com>
2024-07-25 12:19:14 +0330

    Play 5 rounds by calling playRound 5 times and counting scores.

--- javascript.js
+++ javascript.js
@@ -17,35 +17,51 @@ function getHumanChoice(){
     return choice.toLowerCase();
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame(){
 
-function playRound(humanChoice, computerChoice){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice){
+    
+        console.log(`Human:${humanChoice}`);
+        console.log(`Computer:${computerChoice}`);
+    
+        if ( humanChoice==="rock" && computerChoice==="paper"){
+            console.log("You lose! Paper beats Rock.");
+            computerScore+=1;
+        } else if( humanChoice==="rock" && computerChoice==="scissors"){
+            console.log("You win! Rock beats Scissors");
+            humanScore+=1;
+        }  else if(humanChoice==="paper" && computerChoice==="rock"){
+            console.log("You win! Paper beats rock.");
+            humanScore+=1;
+        } else if(humanChoice==="paper"&&computerChoice==="scissors"){
+            console.log("You lose! Scissors beats paper.");
+            computerScore+=1;
+        } else if(humanChoice==="scissors"&&computerChoice==="paper"){
+            console.log("You win! Scissors beats paper.");
+            humanScore+=1;
+        } else if(humanChoice==="scissors"&&computerChoice==="rock"){
+            console.log("You lose! Rock beats scissors.");
+            computerScore+=1;
+        } else if( (humanChoice==="rock" && computerChoice==="rock")||
+        (humanChoice==="paper" && computerChoice==="paper")||
+        (humanChoice==="scissors" && computerChoice==="scissors")){
+            console.log("It's a tie.");
+        }
 
-    console.log(humanChoice);
-    console.log(computerChoice);
-
-    if ( humanChoice==="rock" && computerChoice==="paper"){
-        console.log("You lose! Paper beats Rock.");
-    } else if( humanChoice==="rock" && computerChoice==="scissors"){
-        console.log("You win! Rock beats Scissors");
-    }  else if(humanChoice==="paper" && computerChoice==="rock"){
-        console.log("You win! Paper beats rock.");
-    } else if(humanChoice==="paper"&&computerChoice==="scissors"){
-        console.log("You lose! Scissors beats paper.");
-    } else if(humanChoice==="scissors"&&computerChoice==="paper"){
-        console.log("You win! Scissors beats paper.");
-    } else if(humanChoice==="scissors"&&computerChoice==="rock"){
-        console.log("You lose! Rock beats scissors.");
-    } else if( (humanChoice==="rock" && computerChoice==="rock")||
-    (humanChoice==="paper" && computerChoice==="paper")||
-    (humanChoice==="scissors" && computerChoice==="scissors")){
-        console.log("It's a tie.");
+        console.log(`Scores-> Human:${humanScore} Computer:${computerScore}`)
     }
+    
+    for (let i=1; i<=5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        
+        playRound(humanSelection, computerSelection);
+    }
+    
+    
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
-
+playGame()
